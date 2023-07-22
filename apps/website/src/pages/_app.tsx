9da81e2af6125eab7001dc93343f36bb2c0e0276
Archaeopteryx <46562212+skdhg@users.noreply.github.com>
2023-07-22 22:24:29 +0545

    Update _app.tsx

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import { AppProps } from "next/app";
 import { DatabaseProvider } from "@/lib/context";
 
@@ -9,11 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootApplication({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-      <ThemeProvider>
-        <DatabaseProvider>
-          <Component {...pageProps} />
-        </DatabaseProvider>
-      </ThemeProvider>
+      <DatabaseProvider>
+        <Component {...pageProps} />
+      </DatabaseProvider>
     </div>
   );
 }
